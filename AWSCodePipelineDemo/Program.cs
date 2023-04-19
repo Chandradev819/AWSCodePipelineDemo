084950b498c0fdf45e7c5e25adf5eb8ac3014f18
Chandradev <chandradev819@hotmail.com>
2023-04-19 15:50:07 +0530

    Code has been updated.

--- AWSCodePipelineDemo/Program.cs
+++ AWSCodePipelineDemo/Program.cs
@@ -21,5 +21,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-app.MapGet("/", () => "Hello AWS CICD Pipeline819!");
+app.MapGet("/", () => "Hello AWS CICD Pipeline");
 app.Run();
